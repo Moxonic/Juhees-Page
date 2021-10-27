eb0c2b2c8c4d4dadfe6990ef977ca11aed0eb871
81615380 <d.soto@gmx.at>
2021-10-27 15:55:35 +0200

    first time uploading

---
+++ myparallax.js
@@ -0,0 +1,8 @@
+var juheepeek = document.getElementById('juheepeek');
+var parallaxInstance = new Parallax(juheepeek);
+
+var dust = document.getElementById('dust');
+var parallaxInstance = new Parallax(dust);
+
+var light = document.getElementById('lightCont');
+var parallaxInstance = new Parallax(light);
