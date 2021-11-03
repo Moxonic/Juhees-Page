e72679d50957b02066e843bf5427f0472a03210f
81615380 <d.soto@gmx.at>
2021-11-03 12:56:19 +0100

    adding video

--- lightSwitch.js
+++ lightSwitch.js
@@ -5,18 +5,17 @@ let licht =document.querySelector('.licht');
 
 // let dust = document.querySelector('#dust');
 
-        licht.style.background = "linear-gradient(to right,black, rgba(100, 0, 0, 0.295),black)";
+        licht.style.background = "linear-gradient(to right,rgba(0, 0, 0, 0.853) 15%, rgba(100, 0, 0, 0.295),rgba(0, 0, 0, 0.853) 85%)";
         dust.style.display = 'none';
 
 btn.onclick = ()=>{
     console.log('knopft')
     if(dust.style.display === 'none'){
     dust.style.display = 'block';
-    licht.style.background = 'linear-gradient(to right,black, rgba(255, 0, 0, 0.295), rgba(58, 216, 255, 0.37), rgba(255, 0, 200, 0.521),black)'; 
+    licht.style.background = 'linear-gradient(to right,rgba(0, 0, 0, 0.853) , rgba(255, 0, 0, 0.295), rgba(58, 216, 255, 0.37), rgba(255, 0, 200, 0.521),rgba(0, 0, 0, 0.853) )'; 
 
     }else{
         dust.style.display = 'none';
-        licht.style.background = "linear-gradient(to right,black, rgba(100, 0, 0, 0.295),black)";
-
+        licht.style.background = "linear-gradient(to right,rgba(0, 0, 0, 0.853) , rgba(100, 0, 0, 0.295),rgba(0, 0, 0, 0.853) )";
     };
 }
